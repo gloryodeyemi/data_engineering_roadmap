ef7beee6ab4f441cd179d403a0790dbf91e21328
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-10-28 13:53:26 -0400

    SQL select

---
+++ SQL/Zero-Hero_Bootcamp/SQL-select.sql
@@ -0,0 +1,12 @@
+SELECT * FROM actor;
+
+SELECT first_name FROM actor;
+
+SELECT first_name, last_name FROM actor;
+
+SELECT * FROM city;
+
+-- Challenge
+-- Use a select statement to grab the first and last names of every customer and their email 
+-- address.
+SELECT first_name, last_name, email FROM customer;
