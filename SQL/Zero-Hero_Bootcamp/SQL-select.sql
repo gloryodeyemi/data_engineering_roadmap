c475bc79779262a60871c903e838ef7ad194fc01
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-10-28 16:05:35 -0400

    SQL select distinct

--- SQL/Zero-Hero_Bootcamp/SQL-select.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-select.sql
@@ -1,3 +1,4 @@
+-- SELECT
 SELECT * FROM actor;
 
 SELECT first_name FROM actor;
@@ -10,3 +11,16 @@ SELECT * FROM city;
 -- Use a select statement to grab the first and last names of every customer and their email 
 -- address.
 SELECT first_name, last_name, email FROM customer;
+
+-- SELECT DISTINCT
+SELECT DISTINCT release_year FROM film;
+
+SELECT DISTINCT(release_year) FROM film;
+
+SELECT DISTINCT(rental_rate) FROM film;
+
+-- Challenge
+-- Use select distinct to retrieve the distinct rating types our films could have in our 
+-- database. 
+SELECT DISTINCT rating FROM film;
+
