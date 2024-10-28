7b8a43fbba06ffe7b482d158ee2104e226f75cd8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-10-28 18:13:03 -0400

    SQL limit

--- SQL/Zero-Hero_Bootcamp/SQL-orderby-limit.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-orderby-limit.sql
@@ -16,3 +16,19 @@ SELECT * FROM payment
 WHERE amount != 0.0
 ORDER BY payment_date DESC
 LIMIT 5;
+
+-- Challenge
+-- 1. What are the customer ids of the first 10 customers who created a payment?
+SELECT customer_id FROM payment
+WHERE amount != 0.0
+ORDER BY payment_date ASC
+LIMIT 10;
+
+-- 2. What are the titles of the 5 shortest (in length of runtime) movies;
+SELECT title, length FROM film
+ORDER BY length ASC
+LIMIT 5;
+
+-- 3. How many movies can a customer watch that is 50 minutes or less in runtime?
+SELECT COUNT(*) FROM film
+WHERE length <= 50;
