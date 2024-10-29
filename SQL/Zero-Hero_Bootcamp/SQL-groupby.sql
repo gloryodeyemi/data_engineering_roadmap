12e656276fe090477b255366c2dc6e8a02496053
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-10-29 17:03:53 -0400

    SQL aggregate functions

--- SQL/Zero-Hero_Bootcamp/SQL-groupby.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-groupby.sql
@@ -1,5 +1,16 @@
 -- AGGREGATE FUNCTIONS
 -- AVG(), COUNT(), MAX(), MIN(), SUM()
 -- Happens only in the SELECT or HAVING clause
+SELECT MIN(replacement_cost) FROM film;
+
+SELECT MAX(replacement_cost) FROM film;
+
+SELECT MAX(replacement_cost), MIN(replacement_cost) FROM film;
+
+SELECT COUNT(*) FROM film;
+
+SELECT ROUND(AVG(replacement_cost), 2) FROM film;
+
+SELECT SUM(replacement_cost) FROM film;
 
 -- GROUP BY
