b2bee520452fecd9818108ccf47d3765b21c3bdb
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-10-29 17:32:54 -0400

    SQL group by

--- SQL/Zero-Hero_Bootcamp/SQL-groupby.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-groupby.sql
@@ -14,3 +14,32 @@ SELECT ROUND(AVG(replacement_cost), 2) FROM film;
 SELECT SUM(replacement_cost) FROM film;
 
 -- GROUP BY
+-- Must appear right after a FROM or WHERE statement
+-- Column selected must appear in the group by clause except if it has an aggregate function.
+-- WHERE statement should not refer to the aggregation result.
+-- Sorting result based on aggregate must reference the entire function
+SELECT customer_id FROM payment
+GROUP BY customer_id
+ORDER BY customer_id;
+
+-- What customer is spending the most money in total?
+SELECT customer_id, SUM(amount) FROM payment
+GROUP BY customer_id
+ORDER BY SUM(amount) DESC;
+
+-- How many transactions per customer
+SELECT customer_id, COUNT(amount) FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(amount) DESC;
+
+SELECT customer_id, staff_id, SUM(amount) FROM payment
+GROUP BY staff_id, customer_id
+ORDER BY customer_id;
+
+SELECT staff_id, customer_id, SUM(amount) FROM payment
+GROUP BY staff_id, customer_id
+ORDER BY staff_id, customer_id;
+
+SELECT DATE(payment_date), SUM(amount) FROM payment
+GROUP BY DATE(payment_date)
+ORDER BY SUM(amount) DESC;
