d35f6a12b607dc1fea387591968093d9e21414a8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-12-06 11:39:14 -0500

    Date and time functions

--- SQL/Zero-Hero_Bootcamp/SQL-time-functions.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-time-functions.sql
@@ -16,3 +16,39 @@ SELECT TIMEOFDAY();
 SELECT CURRENT_TIME;
 
 SELECT CURRENT_DATE;
+
+-- EXTRACT()
+-- allows you to extract or obtain a sub-component of a date value.
+-- YEAR, MONTH, DAY, WEEK, QUARTER.
+SELECT EXTRACT(YEAR FROM payment_date) AS payyear
+FROM payment;
+
+SELECT EXTRACT(MONTH FROM payment_date) AS paymonth
+FROM payment;
+
+SELECT EXTRACT(QUARTER FROM payment_date) AS payquarter
+FROM payment;
+
+
+-- AGE()
+-- calculates and returns the current age given a timestamp.
+SELECT AGE(payment_date)
+FROM payment;
+
+-- TO_CHAR()
+-- general function to convert data types to text. It is useful for timestamp 
+-- formatting.
+SELECT TO_CHAR(payment_date, 'MONTH-YYYY')
+FROM payment;
+
+SELECT TO_CHAR(payment_date, 'mon/dd/yyyy')
+FROM payment;
+
+SELECT TO_CHAR(payment_date, 'mm/dd/yyyy')
+FROM payment;
+
+SELECT TO_CHAR(payment_date, 'mm-dd-yyyy')
+FROM payment;
+
+SELECT TO_CHAR(payment_date, 'dd-mm-yyyy')
+FROM payment;
