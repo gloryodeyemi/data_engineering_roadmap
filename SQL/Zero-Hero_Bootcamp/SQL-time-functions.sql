dbde652d267eda88223c9b1c0f5c93bd63ba1db8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-12-06 11:52:51 -0500

    Date and time functions challenges

--- SQL/Zero-Hero_Bootcamp/SQL-time-functions.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-time-functions.sql
@@ -52,3 +52,14 @@ FROM payment;
 
 SELECT TO_CHAR(payment_date, 'dd-mm-yyyy')
 FROM payment;
+
+-- Challenge
+-- 1. During which months did payments occur?
+-- Format your answer to return back the full month name.
+SELECT DISTINCT TO_CHAR(payment_date, 'MONTH') AS payment_months
+FROM payment;
+
+-- 2. How many payments occurred on a Monday?
+SELECT COUNT(EXTRACT(DOW FROM payment_date)) AS monday_payments
+FROM payment
+WHERE EXTRACT(DOW FROM payment_date) = 1;
