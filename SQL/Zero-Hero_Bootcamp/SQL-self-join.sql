0879ea8f0ca2407d93883c7e678cb7e63aeb1044
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-12-06 13:00:07 -0500

    Self join

--- SQL/Zero-Hero_Bootcamp/SQL-self-join.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-self-join.sql
@@ -1,2 +1,7 @@
 -- SELF JOIN
 -- a query in which a table is joined to itself.
+SELECT f1.title, f2.title, f1.length
+FROM film AS f1
+INNER JOIN film AS f2
+ON f1.film_id != f2.film_id
+AND f1.length = f2.length;
