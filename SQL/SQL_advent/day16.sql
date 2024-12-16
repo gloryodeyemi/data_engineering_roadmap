8f25ce92aac9119e6d3bcb786169a400d205ac71
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2024-12-16 14:22:25 -0500

    Day 16 - SQL advent calendar

--- SQL/SQL_advent/day16.sql
+++ SQL/SQL_advent/day16.sql
@@ -1,7 +1,8 @@
 -- Day 16 of SQL Advent Calendar
 
 -- Today's Question:
--- As the owner of a candy store, you want to understand which of your products are selling best. Write a query to calculate the total revenue generated from each candy category.
+-- As the owner of a candy store, you want to understand which of your products are selling best. Write a query to calculate 
+-- the total revenue generated from each candy category.
 
 -- Table name: candy_sales
 -- sale_id	candy_name	quantity_sold	price_per_unit	category
