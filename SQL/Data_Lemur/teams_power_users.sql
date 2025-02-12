33d1d45d8635d860aeb82ec7eb5e837b46f81d03
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-12 13:37:24 -0500

    Teams Power Users

--- SQL/Data_Lemur/teams_power_users.sql
+++ SQL/Data_Lemur/teams_power_users.sql
@@ -34,6 +34,9 @@ SELECT sender_id, COUNT(*) AS message_count
 FROM messages
 WHERE EXTRACT(MONTH FROM sent_date) = '8'
 AND EXTRACT(YEAR FROM sent_date) = '2022'
+-- WHERE sent_date BETWEEN '08/01/2022' AND '08/31/2022'
+-- WHERE sent_date >= '08/01/2022' 
+-- AND sent_date <= '08/31/2022'
 GROUP BY sender_id
 ORDER BY COUNT(*) DESC
 LIMIT 2;
