43af5790086378974707778388b4295c2da63d82
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-15 02:59:49 -0500

    CASE challenge

--- SQL/Zero-Hero_Bootcamp/SQL-case.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-case.sql
@@ -30,3 +30,20 @@ SUM(CASE rental_rate
 	ELSE 0
 END) AS premium
 FROM film;
+
+-- Challenge Tasks
+-- 1. We want to know and compare the various amounts of films we have per movie rating.
+SELECT
+SUM(CASE rating
+	WHEN 'R' THEN 1
+	ELSE 0
+END) AS r,
+SUM(CASE rating
+	WHEN 'PG' THEN 1
+	ELSE 0
+END) AS pg,
+SUM(CASE rating
+	WHEN 'PG-13' THEN 1
+	ELSE 0
+END) AS pg13
+FROM film;
