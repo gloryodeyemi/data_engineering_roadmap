29858649b4e4174e686058031ddb80168c933476
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-15 03:45:05 -0500

    VIEW

--- SQL/Zero-Hero_Bootcamp/SQL-view.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-view.sql
@@ -2,3 +2,24 @@
 -- a database object that is of a stored query.
 -- it can be accessed as a virtual table in PostgreSQL.
 -- you can update and alter existing views.
+
+CREATE VIEW customer_info AS
+	SELECT first_name, last_name, address
+	FROM customer
+	INNER JOIN address
+	ON customer.address_id = address.address_id;
+
+SELECT * FROM customer_info;
+
+-- ALTER VIEW
+CREATE OR REPLACE VIEW customer_info AS
+	SELECT first_name, last_name, address, district
+	FROM customer
+	INNER JOIN address
+	ON customer.address_id = address.address_id;
+
+-- change VIEW name
+ALTER VIEW customer_info RENAME TO c_info;
+
+-- remove VIEW
+DROP VIEW IF EXISTS c_info;
