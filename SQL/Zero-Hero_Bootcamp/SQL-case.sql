5166fa432c76a83376fda810a2693fd0dbcd46c2
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-15 02:54:43 -0500

    CASE

--- SQL/Zero-Hero_Bootcamp/SQL-case.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-case.sql
@@ -4,5 +4,29 @@ CASE
 	WHEN (customer_id <= 100) THEN 'Premium'
 	WHEN (customer_id BETWEEN 100 AND 200) THEN 'Plus'
 	ELSE 'Normal'
-END 
+END AS customer_class
 FROM customer;
+
+-- CASE expression
+SELECT customer_id,
+CASE customer_id
+	WHEN 2 THEN 'Winner'
+	WHEN 5 THEN 'Second Place'
+	ELSE 'Normal'
+END AS raffle_results
+FROM customer;
+
+SELECT
+SUM(CASE rental_rate
+	WHEN 0.99 THEN 1
+	ELSE 0
+END) AS bargains,
+SUM(CASE rental_rate
+	WHEN 2.99 THEN 1
+	ELSE 0
+END) AS regular,
+SUM(CASE rental_rate
+	WHEN 4.99 THEN 1
+	ELSE 0
+END) AS premium
+FROM film;
