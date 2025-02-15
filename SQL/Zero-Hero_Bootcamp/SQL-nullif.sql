74142464ed6ec434b469eef3c684c9a0f62fa2d3
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-15 03:35:30 -0500

    NULLIF

--- SQL/Zero-Hero_Bootcamp/SQL-nullif.sql
+++ SQL/Zero-Hero_Bootcamp/SQL-nullif.sql
@@ -21,3 +21,10 @@ FROM depts;
 
 DELETE FROM depts
 WHERE department = 'B';
+
+-- using NULLIF
+SELECT (
+	SUM(CASE WHEN department = 'A' THEN 1 ELSE 0 END)/
+	NULLIF(SUM(CASE WHEN department = 'B' THEN 1 ELSE 0 END), 0)
+) AS department_ratio
+FROM depts;
