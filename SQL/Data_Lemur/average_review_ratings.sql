e4e5af74c5c145d93ce13e1a72843f33a019745d
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-17 18:19:21 -0500

    Average Review Ratings

--- SQL/Data_Lemur/average_review_ratings.sql
+++ SQL/Data_Lemur/average_review_ratings.sql
@@ -32,9 +32,9 @@
 
 -- Solution
 SELECT 
-  EXTRACT(MONTH FROM submit_date) AS month,
+  EXTRACT(MONTH FROM submit_date) AS order_month,
   product_id,
   ROUND(AVG(stars), 2) AS avg_stars
 FROM reviews
 GROUP BY EXTRACT(MONTH FROM submit_date), product_id
-ORDER BY month, product_id;
+ORDER BY order_month, product_id;
