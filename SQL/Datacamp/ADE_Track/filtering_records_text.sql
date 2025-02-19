dcfaee85597111625b985f52423b6b0445360838
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-18 20:30:16 -0500

    Combining filtering and selecting

--- SQL/Datacamp/ADE_Track/filtering_records_text.sql
+++ SQL/Datacamp/ADE_Track/filtering_records_text.sql
@@ -31,3 +31,5 @@ SELECT title, certification, language
 FROM films
 WHERE certification IN ('NC-17', 'R')
     AND language IN ('English', 'Italian', 'Greek');
+
+-- Combining filtering and selecting
