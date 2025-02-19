99a191efdf75cbcb936965df2e8c31d1dd8886ba
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-19 17:46:18 -0500

    Aggregate functions

--- SQL/Datacamp/ADE_Track/aggregate_functions.sql
+++ SQL/Datacamp/ADE_Track/aggregate_functions.sql
@@ -15,3 +15,23 @@ FROM films;
 -- Find the duration of the shortest film and use the alias shortest_film.
 SELECT MIN(duration) AS shortest_film
 FROM films;
+
+-- Use SUM() to calculate the total gross for all films made in the year 2000 or later, and use the alias total_gross.
+SELECT SUM(gross) AS total_gross
+FROM films
+WHERE release_year >= 2000;
+
+-- Calculate the average amount grossed by all films whose titles start with the letter 'A' and alias with avg_gross_A.
+SELECT AVG(gross) AS avg_gross_A
+FROM films
+WHERE title LIKE 'A%';
+
+-- Calculate the lowest gross film in 1994 and use the alias lowest_gross.
+SELECT MIN(gross) AS lowest_gross
+FROM films
+WHERE release_year = 1994;
+
+-- Calculate the highest gross film between 2000 and 2012, inclusive, and use the alias highest_gross.
+SELECT MAX(gross) AS highest_gross
+FROM films
+WHERE release_year BETWEEN 2000 AND 2012;
