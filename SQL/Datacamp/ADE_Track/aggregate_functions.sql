37323efa9f91c42be12b614fea56b25f2173cb9a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-19 18:00:51 -0500

    Aggregate functions

--- SQL/Datacamp/ADE_Track/aggregate_functions.sql
+++ SQL/Datacamp/ADE_Track/aggregate_functions.sql
@@ -43,3 +43,19 @@ FROM reviews;
 -- Calculate the average budget from the films table, aliased as avg_budget_thousands, and round to the nearest thousand.
 SELECT ROUND(AVG(budget), -3) AS avg_budget_thousands
 FROM films;
+
+-- Select the title and duration in hours for all films and alias as duration_hours; since the current durations are in minutes, you'll need to divide duration by 60.0.
+SELECT title, (duration/60.0) AS duration_hours
+FROM films;
+
+-- Calculate the percentage of people who are no longer alive and alias the result as percentage_dead.
+SELECT COUNT(deathdate) * 100.0 / COUNT(*) AS percentage_dead
+FROM people;
+
+-- Find how many decades (period of ten years) the films table covers by using MIN() and MAX(); alias as number_of_decades.
+SELECT (MAX(release_year) - MIN(release_year)) / 10.0 AS number_of_decades
+FROM films;
+
+-- Update the query by adding ROUND() around the calculation and round to two decimal places.
+SELECT title, ROUND(duration / 60.0, 2) AS duration_hours
+FROM films;
