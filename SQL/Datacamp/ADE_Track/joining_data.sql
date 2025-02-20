b034ae3b9afdbe62e3cf666ec08022817b73c140
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-20 16:09:11 -0500

    Joining

--- SQL/Datacamp/ADE_Track/joining_data.sql
+++ SQL/Datacamp/ADE_Track/joining_data.sql
@@ -13,3 +13,11 @@ SELECT cities.name AS city, countries.name AS country, region
 FROM cities
 INNER JOIN countries
 ON cities.country_code = countries.code;
+
+-- Start with your inner join in line 5; join the tables countries AS c (left) with economies (right), aliasing economies AS e.
+-- Next, use code as your joining field in line 7; do not use the USING command here.
+-- Lastly, select the following columns in order in line 2: code from the countries table (aliased as country_code), name, year, and inflation_rate.
+SELECT c.code AS country_code, name, year, inflation_rate
+FROM countries AS c
+INNER JOIN economies AS e
+ON c.code = e.code;
