10509ecb18e36224dedd4f227fe0dace694e75cb
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-19 19:35:35 -0500

    Sorting and Grouping

--- SQL/Datacamp/ADE_Track/sorting_and_grouping.sql
+++ SQL/Datacamp/ADE_Track/sorting_and_grouping.sql
@@ -9,3 +9,13 @@ ORDER BY name ASC;
 SELECT title, duration
 FROM films
 ORDER BY duration DESC;
+
+-- Select the release_year, duration, and title of films ordered by their release year and duration, in that order.
+SELECT release_year, duration, title
+FROM films
+ORDER BY release_year, duration;
+
+-- Select the certification, release_year, and title from films ordered first by certification (alphabetically) and second by release year, starting with the most recent year.
+SELECT certification, release_year, title
+FROM films
+ORDER BY certification, release_year DESC;
