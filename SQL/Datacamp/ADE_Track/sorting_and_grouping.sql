67479af0a23fe7540f9be77143ee7c2eaeef5d6a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-19 19:50:04 -0500

    Sorting and Grouping

--- SQL/Datacamp/ADE_Track/sorting_and_grouping.sql
+++ SQL/Datacamp/ADE_Track/sorting_and_grouping.sql
@@ -35,3 +35,9 @@ SELECT release_year, country, MAX(budget) AS max_budget
 FROM films
 GROUP BY release_year, country
 ORDER BY release_year, country;
+
+-- Using the films table: which release_year had the most language diversity?
+SELECT release_year, COUNT(DISTINCT language) AS language_diversity
+FROM films
+GROUP BY release_year
+ORDER BY language_diversity DESC;
