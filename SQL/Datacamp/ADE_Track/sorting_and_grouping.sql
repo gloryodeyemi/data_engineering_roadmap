a598b75960de5286aadc9e7295a6a20a12dc1c57
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-19 19:47:03 -0500

    Sorting and Grouping

--- SQL/Datacamp/ADE_Track/sorting_and_grouping.sql
+++ SQL/Datacamp/ADE_Track/sorting_and_grouping.sql
@@ -29,3 +29,9 @@ GROUP BY release_year;
 SELECT release_year, AVG(duration) AS avg_duration
 FROM films
 GROUP BY release_year;
+
+-- Select the release_year, country, and the maximum budget aliased as max_budget for each year and each country; sort your results by release_year and country.
+SELECT release_year, country, MAX(budget) AS max_budget
+FROM films
+GROUP BY release_year, country
+ORDER BY release_year, country;
