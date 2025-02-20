beb86921423853195052e3b5a0125ed3200c415b
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-20 17:24:25 -0500

    Joining

--- SQL/Datacamp/ADE_Track/joining_data.sql
+++ SQL/Datacamp/ADE_Track/joining_data.sql
@@ -55,3 +55,52 @@ INNER JOIN economies AS e
 USING(code);
 -- Modify your query so that you are joining to economies on year as well as code.
     AND p.year = e.year;
+
+--  LEFT and RIGHT JOINs
+
+-- Perform an inner join with cities AS c1 on the left and countries as c2 on the right.
+-- Use code as the field to merge your tables on.
+SELECT 
+    c1.name AS city,
+    code,
+    c2.name AS country,
+    region,
+    city_proper_pop
+FROM cities AS c1
+INNER JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+
+-- Change the code to perform a LEFT JOIN instead of an INNER JOIN.
+-- After executing this query, have a look at how many records the query result contains.
+SELECT 
+	c1.name AS city, 
+    code, 
+    c2.name AS country,
+    region, 
+    city_proper_pop
+FROM cities AS c1
+LEFT JOIN countries as c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+
+-- Complete the LEFT JOIN with the countries table on the left and the economies table on the right on the code field.
+-- Filter the records from the year 2010.
+SELECT name, region, gdp_percapita
+FROM countries AS c
+LEFT JOIN economies AS e
+ON c.code = e.code
+WHERE year = 2010;
+
+-- To calculate per capita GDP per region, begin by grouping by region.
+-- After your GROUP BY, choose region in your SELECT statement, followed by average GDP per capita using the AVG() function, with AS avg_gdp as your alias.
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+GROUP BY region;
+-- Order the result set by the average GDP per capita from highest to lowest.
+-- Return only the first 10 records in your result.
+ORDER BY avg_gdp DESC
+LIMIT 10;
