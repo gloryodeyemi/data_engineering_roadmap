8e1307a045cc76683840173118a51d3e9b5d1c60
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-20 17:28:43 -0500

    Joining

--- SQL/Datacamp/ADE_Track/joining_data.sql
+++ SQL/Datacamp/ADE_Track/joining_data.sql
@@ -104,3 +104,10 @@ GROUP BY region;
 -- Return only the first 10 records in your result.
 ORDER BY avg_gdp DESC
 LIMIT 10;
+
+-- Write a new query using RIGHT JOIN that produces an identical result to the LEFT JOIN provided.
+SELECT countries.name AS country, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+USING(code)
+ORDER BY language;
