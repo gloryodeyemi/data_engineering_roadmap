3c569e9fa94f0e749b5671d4aae37a05f7f206d8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-24 17:30:47 -0500

    Relational databases

--- SQL/Datacamp/ADE_Track/relational_databases.sql
+++ SQL/Datacamp/ADE_Track/relational_databases.sql
@@ -143,3 +143,16 @@ SET id = CONCAT(make, model);
 -- Make id a primary key and name it id_pk.
 ALTER TABLE cars
 ADD CONSTRAINT id_pk PRIMARY KEY(id);
+
+-- Let's think of an entity type "student". A student has:
+-- a last name consisting of up to 128 characters (required),
+-- a unique social security number, consisting only of integers, that should serve as a key,
+-- a phone number of fixed length 12, consisting of numbers and characters (but some students don't have one).
+-- Given the above description of a student entity, create a table students with the correct column types.
+-- Add a PRIMARY KEY for the social security number ssn.
+-- Note that there is no formal length requirement for the integer column. The application would have to make sure it's a correct SSN!
+CREATE TABLE students (
+  last_name VARCHAR(128) NOT NULL,
+  ssn INTEGER PRIMARY KEY,
+  phone_no CHAR(12)
+);
