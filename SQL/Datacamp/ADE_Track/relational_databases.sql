46fd1f68e2f016b5e6b140f201c449cfc5695bee
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-24 17:26:48 -0500

    Relational databases

--- SQL/Datacamp/ADE_Track/relational_databases.sql
+++ SQL/Datacamp/ADE_Track/relational_databases.sql
@@ -86,3 +86,60 @@ ADD CONSTRAINT university_shortname_unq UNIQUE(university_shortname);
 -- Add a unique constraint to the organization column in organizations. Give it the name organization_unq.
 ALTER TABLE organizations
 ADD CONSTRAINT organization_unq UNIQUE(organization);
+
+-- First, find out the number of rows in universities.
+SELECT COUNT(*) 
+FROM universities;
+
+-- Then, find out how many unique values there are in the university_city column.
+SELECT COUNT(DISTINCT(university_city)) 
+FROM universities;
+
+-- Identify the candidate key by trying out different combination of columns.
+SELECT COUNT(DISTINCT(firstname, lastname)) 
+FROM professors;
+
+-- Rename the organization column to id in organizations.
+-- Make id a primary key and name it organization_pk.
+ALTER TABLE organizations
+RENAME COLUMN organization TO id;
+
+ALTER TABLE organizations
+ADD CONSTRAINT organization_pk PRIMARY KEY (id);
+
+-- Rename the university_shortname column to id in universities.
+-- Make id a primary key and name it university_pk.
+ALTER TABLE universities
+RENAME COLUMN university_shortname TO id;
+
+ALTER TABLE universities
+ADD CONSTRAINT university_pk PRIMARY KEY (id);
+
+-- Add a new column id with data type serial to the professors table.
+ALTER TABLE professors 
+ADD COLUMN id SERIAL;
+
+-- Make id a primary key and name it professors_pkey.
+ALTER TABLE professors 
+ADD CONSTRAINT professors_pkey PRIMARY KEY (id);
+
+-- Write a query that returns all the columns and 10 rows from professors.
+SELECT *
+FROM professors
+LIMIT 10;
+
+-- Count the number of distinct rows with a combination of the make and model columns.
+SELECT COUNT(DISTINCT(make, model))
+FROM cars;
+
+-- Add a new column id with the data type varchar(128).
+ALTER TABLE cars
+ADD COLUMN id VARCHAR(128);
+
+-- Concatenate make and model into id using an UPDATE table_name SET column_name = ... query and the CONCAT() function.
+UPDATE cars
+SET id = CONCAT(make, model);
+
+-- Make id a primary key and name it id_pk.
+ALTER TABLE cars
+ADD CONSTRAINT id_pk PRIMARY KEY(id);
