ccb427001bf5dfeb992300c6d8fa0f9d95d6a772
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-24 18:31:03 -0500

    Relational databases

--- SQL/Datacamp/ADE_Track/relational_databases.sql
+++ SQL/Datacamp/ADE_Track/relational_databases.sql
@@ -200,3 +200,47 @@ DROP COLUMN firstname;
 
 ALTER TABLE affiliations
 DROP COLUMN lastname;
+
+-- Have a look at the existing foreign key constraints by querying table_constraints in information_schema.
+SELECT constraint_name, table_name, constraint_type
+FROM information_schema.table_constraints
+WHERE constraint_type = 'FOREIGN KEY';
+
+-- Delete the affiliations_organization_id_fkey foreign key constraint in affiliations.
+ALTER TABLE affiliations
+DROP CONSTRAINT affiliations_organization_id_fkey;
+
+-- Add a new foreign key to affiliations that CASCADEs deletion if a referenced record is deleted from organizations. Name it affiliations_organization_id_fkey.
+ALTER TABLE affiliations
+ADD CONSTRAINT affiliations_organization_id_fkey FOREIGN KEY (organization_id) REFERENCES organizations (id) ON DELETE CASCADE;
+
+-- Count the number of total affiliations by university.
+-- Sort the result by that count, in descending order.
+SELECT COUNT(*), professors.university_id 
+FROM affiliations
+JOIN professors
+ON affiliations.professor_id = professors.id
+GROUP BY professors.university_id 
+ORDER BY count DESC;
+
+-- Find the university city of the professor with the most affiliations in the sector "Media & communication".
+-- Join all tables in the database (starting with affiliations, professors, organizations, and universities) and look at the result.
+-- Group the result by organization sector, professor, and university city.
+-- Count the resulting number of rows.
+-- Only retain rows with "Media & communication" as organization sector, and sort the table by count, in descending order.
+SELECT 
+    COUNT(*), 
+    organizations.organization_sector, 
+    professors.id, 
+    universities.university_city
+FROM affiliations
+JOIN professors
+    ON affiliations.professor_id = professors.id
+JOIN organizations
+    ON affiliations.organization_id = organizations.id
+JOIN universities
+    ON professors.university_id = universities.id
+WHERE organizations.organization_sector = 'Media & communication'
+GROUP BY organizations.organization_sector, 
+    professors.id, universities.university_city
+ORDER BY COUNT DESC;
