4e27db9cbd494edd1da309cbf97eeb01c302ca8f
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-25 14:51:32 -0500

    Database Design

---
+++ SQL/Datacamp/ADE_Track/database_design.sql
@@ -0,0 +1,4 @@
+--  Dimension modeling
+
+-- Create a dimension table called route that will hold the route information.
+-- Create a dimension table called week that will hold the week information.
