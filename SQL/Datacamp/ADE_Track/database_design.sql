c7db7d905cb7ed0fb0e0bc7457559e3a9157d77f
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-25 15:17:50 -0500

    Database Design

--- SQL/Datacamp/ADE_Track/database_design.sql
+++ SQL/Datacamp/ADE_Track/database_design.sql
@@ -30,3 +30,26 @@ WHERE month = 'July' and year = '2019';
 -- In the constraint called sales_book, set book_id as a foreign key.
 -- In the constraint called sales_time, set time_id as a foreign key.
 -- In the constraint called sales_store, set store_id as a foreign key.
+ALTER TABLE fact_booksales ADD CONSTRAINT sales_book
+    FOREIGN KEY (book_id) REFERENCES dim_book_star (book_id);
+    
+ALTER TABLE fact_booksales ADD CONSTRAINT sales_time
+    FOREIGN KEY (time_id) REFERENCES dim_time_star (time_id);
+    
+ALTER TABLE fact_booksales ADD CONSTRAINT sales_store
+    FOREIGN KEY (store_id) REFERENCES dim_store_star (store_id);
+
+-- Create dim_author with a column for author.
+-- Insert all the distinct authors from dim_book_star into dim_author.
+CREATE TABLE dim_author (
+    author VARCHAR(256)  NOT NULL
+);
+
+INSERT INTO dim_author
+SELECT DISTINCT author FROM dim_book_star;
+
+-- Alter dim_author to have a primary key called author_id.
+-- Output all the columns of dim_author.
+ALTER TABLE dim_author ADD COLUMN author_id SERIAL PRIMARY KEY;
+
+SELECT * FROM dim_author;
