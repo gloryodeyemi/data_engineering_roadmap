f615d060c529a95a363120c85f8020b02675d717
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-25 18:01:07 -0500

    Database Design

--- SQL/Datacamp/ADE_Track/database_design.sql
+++ SQL/Datacamp/ADE_Track/database_design.sql
@@ -100,3 +100,60 @@ ADD COLUMN continent_id int NOT NULL DEFAULT(1);
 
 ALTER TABLE dim_country_sf ADD CONSTRAINT country_continent
    FOREIGN KEY (continent_id) REFERENCES dim_continent_sf(continent_id);
+
+-- cars_rented holds one or more car_ids and invoice_id holds multiple values. Create a new table to hold individual car_ids and invoice_ids of the customer_ids 
+-- who've rented those cars.
+-- Drop two columns from customers table to satisfy 1NF
+-- Create a new table to hold the cars rented by customers
+CREATE TABLE cust_rentals (
+  customer_id INT NOT NULL,
+  car_id VARCHAR(128) NULL,
+  invoice_id VARCHAR(128) NULL
+);
+
+ALTER TABLE customers
+DROP COLUMN cars_rented,
+DROP COLUMN invoice_id;
+
+-- Create a new table for the non-key columns that were conflicting with 2NF criteria.
+-- Drop those non-key columns from customer_rentals.
+CREATE TABLE cars (
+  car_id VARCHAR(256) NULL,
+  model VARCHAR(128),
+  manufacturer VARCHAR(128),
+  type_car VARCHAR(128),
+  condition VARCHAR(128),
+  color VARCHAR(128)
+);
+
+-- Insert data into the new table
+INSERT INTO cars
+SELECT DISTINCT
+  car_id,
+  model,
+  manufacturer,
+  type_car,
+  condition,
+  color
+FROM customer_rentals;
+
+-- Drop columns in customer_rentals to satisfy 2NF
+ALTER TABLE customer_rentals
+DROP COLUMN model,
+DROP COLUMN manufacturer, 
+DROP COLUMN type_car,
+DROP COLUMN condition,
+DROP COLUMN color;
+
+-- Create a new table for the non-key columns that were conflicting with 3NF criteria.
+-- Drop those non-key columns from rental_cars.
+CREATE TABLE car_model(
+  model VARCHAR(128),
+  manufacturer VARCHAR(128),
+  type_car VARCHAR(128)
+);
+
+-- Drop columns in rental_cars to satisfy 3NF
+ALTER TABLE rental_cars
+DROP COLUMN model, 
+DROP COLUMN manufacturer;
