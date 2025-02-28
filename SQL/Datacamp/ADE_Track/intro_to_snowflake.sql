9387e59447fe2eb43db70af9c1ffb420bf561d2e
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-02-28 14:24:15 -0500

    Snowflake type conversion

--- SQL/Datacamp/ADE_Track/intro_to_snowflake.sql
+++ SQL/Datacamp/ADE_Track/intro_to_snowflake.sql
@@ -9,3 +9,15 @@ SELECT COUNT(*) AS count_all_pizzas
 FROM pizza_type
 WHERE category = 'Classic'
     AND name LIKE '%Cheese%';
+
+-- Select and convert the request_id column from uber_request_data table to VARCHAR using the CAST method aliasing it to request_id_string.
+-- Convert request_timestamp to DATE using the TO_DATE function aliasing it as request_date.
+-- Convert the drop_timestamp column to TIME using the :: operator, and alias it to drop_time.
+-- Filter the records where request_date is greater than '2016-06-01' and drop_time is less than '6 AM'.
+SELECT 
+    CAST(request_id AS VARCHAR) AS request_id_string,
+    TO_DATE(request_timestamp) AS request_date,
+    drop_timestamp::TIME AS drop_time
+FROM uber_request_data
+WHERE request_date > '2016-06-01'
+AND drop_time < '06:00:00';
