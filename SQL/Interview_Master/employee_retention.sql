a0a97f783a0255c66b0a43700f819ce0ce7f2681
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-13 17:08:49 -0400

    Employee Retention and Satisfaction

---
+++ SQL/Interview_Master/employee_retention.sql
@@ -0,0 +1,9 @@
+-- Employee Retention and Satisfaction
+
+-- As a member of the Netflix HR team, you are tasked with addressing employee retention challenges by analyzing employee satisfaction data. Your goal is to identify the 
+-- average satisfaction scores across different departments and job categories to pinpoint areas for improvement. By understanding these metrics, your team can develop 
+-- targeted strategies to enhance employee engagement and reduce turnover.
+
+-- Question 1 of 3
+-- The HR team wants to understand the average satisfaction score of employees across different departments. Can you provide the average satisfaction score rounded down to the 
+-- nearest whole number for each department?
