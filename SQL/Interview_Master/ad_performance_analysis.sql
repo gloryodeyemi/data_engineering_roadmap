bcb5efa2990956877436a37a1cd068f4f5a2d882
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-13 17:38:02 -0400

    Ad Segment Performance Analysis

---
+++ SQL/Interview_Master/ad_performance_analysis.sql
@@ -0,0 +1,8 @@
+-- Ad Segment Performance Analysis
+
+-- As a Data Analyst on the Marketing Performance team, you are tasked with evaluating the effectiveness of our custom audience segments and lookalike audiences in driving user 
+-- acquisition and conversions. Your team aims to optimize advertising strategies by analyzing key performance metrics such as ad impressions, total conversions, and cost per 
+-- conversion across different audience segments. By leveraging this data, you will provide actionable insights to enhance campaign efficiency and improve overall marketing performance.
+
+-- Question 1 of 3
+-- How many total ad impressions did we receive from custom audience segments in October 2024?
