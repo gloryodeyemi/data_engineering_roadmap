aae618b16e2d6e1496b6dcdee7b7e5c41b2f3dfe
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-17 15:04:44 -0400

    User Engagement in Interactive Content

---
+++ SQL/Interview_Master/engagement.sql
@@ -0,0 +1,8 @@
+-- User Engagement in Interactive Content
+
+-- As a Data Scientist on the Interactive Content team, you are tasked with understanding viewer engagement with our choose-your-own-adventure shows. Your team aims to analyze 
+-- unique viewer interactions, preferences in choices made, and specific interaction types to enhance the design and user experience of future interactive content. By leveraging 
+-- data insights, you will help inform storytelling strategies that foster deeper viewer engagement.
+
+-- Question 1 of 3
+-- The Interactive Content team wants to know how many unique viewers have interacted with any interactive content in October 2024. Can you find out the number of distinct viewers?
