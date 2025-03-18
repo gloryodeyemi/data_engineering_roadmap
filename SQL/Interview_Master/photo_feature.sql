dc6c4740c84f4843e7984b733aaad862849d89a7
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-18 19:23:39 -0400

    User Engagement with Photo Categorization Features

---
+++ SQL/Interview_Master/photo_feature.sql
@@ -0,0 +1,8 @@
+-- User Engagement with Photo Categorization Features
+
+-- As a Data Analyst on the Google Photos Machine Learning Team, you are tasked with understanding user engagement with our automatic photo categorization features. 
+-- Your team aims to quantify how many photos have been categorized and identify the unique users interacting with this functionality. By analyzing this data, 
+-- you will help prioritize enhancements to improve user experience and engagement with our photo management tools.
+
+-- Question 1 of 3
+-- We need to measure initial user engagement with the categorization features. How many photos have been categorized by the system in January 2024?
