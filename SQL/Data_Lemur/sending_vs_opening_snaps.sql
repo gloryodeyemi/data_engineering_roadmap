ac9f31eec51f24c418987a5d1fbe5bfc07092c6c
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-19 18:13:34 -0400

    Sending vs. Opening Snaps

--- SQL/Data_Lemur/sending_vs_opening_snaps.sql
+++ SQL/Data_Lemur/sending_vs_opening_snaps.sql
@@ -53,6 +53,22 @@
 SELECT 
   age_bucket,
   ROUND(
+    SUM(CASE WHEN activity_type = 'send' THEN time_spent ELSE 0 END) * 100.0 / SUM(time_spent), 2
+  ) AS send_perc,
+
+  ROUND(
+    SUM(CASE WHEN activity_type = 'open' THEN time_spent ELSE 0 END) * 100.0 / SUM(time_spent), 2
+  ) AS open_perc
+FROM activities
+INNER JOIN age_breakdown
+  ON activities.user_id = age_breakdown.user_id
+  AND activity_type IN ('send', 'open')
+GROUP BY age_bucket;
+
+-- Solution 2
+SELECT 
+  age_bucket,
+  ROUND(
     SUM(CASE WHEN activity_type = 'send' THEN time_spent ELSE 0 END) * 100.0 /
     NULLIF(SUM(CASE WHEN activity_type IN ('send', 'open') THEN time_spent ELSE 0 END), 0), 2
   ) AS send_perc,
