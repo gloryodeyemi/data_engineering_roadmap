6daff72128019a63630bc32b34a87a56b6c9629b
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-19 18:25:11 -0400

    User Engagement with Search Quality

---
+++ SQL/Interview_Master/user_engagement_with_search.sql
@@ -0,0 +1,8 @@
+-- User Engagement with Search Quality
+
+-- As a Data Analyst on the Google Search Quality team, you are tasked with understanding user engagement with search results. Your goal is to analyze how different user interactions, 
+-- such as clicking on links and spending time on the results page, impact overall satisfaction. By leveraging query data, your team aims to identify areas for improving search result 
+-- relevance and enhancing the user experience.
+
+-- Question 1 of 3
+-- How many search queries were made by users who either clicked on a link or spent more than 30 seconds on the search results page in October 2024?
