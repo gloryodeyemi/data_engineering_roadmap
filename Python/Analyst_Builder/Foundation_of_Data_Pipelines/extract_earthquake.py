cb6f184bf7fab2dd78748d8847b2367a472b533b
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-20 15:22:59 -0400

    extract data from api

---
+++ Python/Analyst_Builder/Foundation_of_Data_Pipelines/extract_earthquake.py
@@ -0,0 +1,60 @@
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+import os
+
+
+def process_earthquake_data():
+    today_date = datetime.now()
+
+    date_15_days_ago = today_date - timedelta(days=15)
+    start_time = date_15_days_ago.strftime("%Y-%m-%d")
+    end_time = today_date.strftime("%Y-%m-%d")
+
+    #  define the API endpoint
+    url = f"https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_time}&endtime={end_time}"
+
+    # send a GET request to the API endpoint
+    response = requests.get(url)
+    # print(response.json())
+    # print(response.status_code)
+
+    # check if the request was successful
+    if response.status_code == 200:
+        # parse the response content as JSON
+        data = response.json()
+
+        # extract earthquake features
+        features = data['features']
+
+        # create a list of dictionaries containing relevan data
+        earthquakes = []
+
+        date = today_date.strftime("%Y_%m_%d")
+
+        filename = f"/Users/new/Downloads/Tutorials/Data_Engineering/DE_Roadmap/Python/Analyst_Builder/Foundation_of_Data_Pipelines/data/earthquake_{date}.csv"
+
+        for feature in features:
+            properties = feature['properties']
+            geometry = feature['geometry']
+            earthquake = {
+                'time': properties['time'],
+                'place': properties['place'],
+                'magnitude': properties['mag'],
+                'longitude': geometry['coordinates'][0],
+                'latitude': geometry['coordinates'][1],
+                'depth': geometry['coordinates'][2],
+                'file_name': filename
+            }
+            earthquakes.append(earthquake)
+        
+        # convert the list of dictionaries to a DataFrame
+        df = pd.DataFrame(earthquakes)
+
+
+def main():
+    process_earthquake_data()
+
+
+if __name__ == "__main__":
+    main()
