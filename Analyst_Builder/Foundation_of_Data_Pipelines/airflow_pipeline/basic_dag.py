6d33bdac8dd8457c630d4e2b12fab3fd08fa17ee
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-21 21:35:12 -0400

    airflow

--- Analyst_Builder/Foundation_of_Data_Pipelines/airflow_pipeline/basic_dag.py
+++ Analyst_Builder/Foundation_of_Data_Pipelines/airflow_pipeline/basic_dag.py
@@ -7,24 +7,63 @@ from airflow.operators.bash import BashOperator
 
 from google.cloud import storage
 
+#  define the default arguments
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
 
-default_args = {}
+
+def failure_callback(context):
+    print(f"Task {context['task_instnce'].task_id} failed")
 
 
-@dag()
+@dag(
+    default_args=default_args,
+    description='A simple DAG to pull data from an API',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2025, 3, 21),
+    catchup=False,
+)
+
+
 def simple_api_dag():
 
-    @task
+    @task(on_failure_callback=failure_callback)
     def pull_data_from_api():
-        url = ""
+        url = "https://data.cityofnewyork.us/resource/h9gi-nx95.json?crash_date=2014-01-21T00:00:00:000"
         response = requests.get(url)
+
+        if response.status_code != 200:
+            raise ValueError("Failed to pull data from API")
         data = response.json()
 
-    t1 = pull_data_from_api()
+        json_path = '/tmp/api_data.json'
+        with open(json_path, 'w') as f:
+            json.dump(data, f)
 
-    t2 = BashOperator(task_id="task_hello_world", bash_command='echo "Hello, World"')
+        # upload to Google Cloud Storage
+        client = storage.Client()
+        bucket = client.bucket()
+        blob = bucket.blob(f'data/api_data.json')
+        blob.upload_from_filename(json_path)
+        os.remove(json_path)
 
-    t1 >> t2
+        print("Data pulled from API and saved to data/api_data.json")
+
+    pull_data_from_api_task = pull_data_from_api()
+
+    bash_task = BashOperator(
+        task_id="task_hello_world", 
+        bash_command='echo "Hello, World"'
+    )
+
+    # managing dependencies
+    pull_data_from_api_task >> bash_task
 
 
 # Instantiate the DAG
