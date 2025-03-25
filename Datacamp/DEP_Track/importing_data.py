d5a8cf0cb4fe396957b2206159a58a5fa44047bd
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-25 17:46:40 -0400

    importing hdf5 file

--- Datacamp/DEP_Track/importing_data.py
+++ Datacamp/DEP_Track/importing_data.py
@@ -182,3 +182,49 @@ pd.DataFrame.hist(df[['disa10']])
 plt.xlabel('Extent of disease')
 plt.ylabel('Number of countries')
 plt.show()
+
+"""
+Import the package h5py.
+Assign the name of the file to the variable file.
+Load the file as read only into the variable data.
+Print the datatype of data.
+Print the names of the groups in the HDF5 file 'LIGO_data.hdf5'.
+"""
+import numpy as np
+import h5py
+
+file = 'LIGO_data.hdf5'
+data = h5py.File(file, 'r')
+print(type(data))
+
+# Print the keys of the file
+for key in data.keys():
+    print(key)
+
+"""
+Assign the HDF5 group data['strain'] to group.
+In the for loop, print out the keys of the HDF5 group in group.
+Assign the time series data data['strain']['Strain'] to a NumPy array called strain.
+Set num_samples equal to 10000, the number of time points we wish to sample.
+Execute the rest of the code to produce a plot of the time series data in LIGO_data.hdf5.
+"""
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = np.array(data['strain']['Strain'])
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
