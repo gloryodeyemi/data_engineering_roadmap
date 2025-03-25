91c8313bc4550b89a6ebce8e21fee5757238f449
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-25 17:51:58 -0400

    importing matlab file

--- Datacamp/DEP_Track/importing_data.py
+++ Datacamp/DEP_Track/importing_data.py
@@ -228,3 +228,37 @@ plt.plot(time, strain[:num_samples])
 plt.xlabel('GPS Time (s)')
 plt.ylabel('strain')
 plt.show()
+
+"""
+Import the package scipy.io.
+Load the file 'albeck_gene_expression.mat' into the variable mat; do so using the function scipy.io.loadmat().
+Use the function type() to print the datatype of mat to the shell.
+"""
+import scipy.io
+
+# Load MATLAB file: mat
+mat = scipy.io.loadmat('albeck_gene_expression.mat')
+print(type(mat))
+
+"""
+Use the method .keys() on the dictionary mat to print the keys. Most of these keys (in fact the ones that do NOT begin and end with '__') are variables from the corresponding MATLAB environment.
+Print the type of the value corresponding to the key 'CYratioCyt' in mat. Recall that mat['CYratioCyt'] accesses the value.
+Print the shape of the value corresponding to the key 'CYratioCyt' using the numpy function shape().
+Execute the entire script to see some oscillatory gene expression data!
+"""
+# Print the keys of the MATLAB dictionary
+print(mat.keys())
+
+# Print the type of the value corresponding to the key 'CYratioCyt'
+print(type(mat['CYratioCyt']))
+
+# Print the shape of the value corresponding to the key 'CYratioCyt'
+print(np.shape(mat['CYratioCyt']))
+
+# Subset the array and plot it
+data = mat['CYratioCyt'][25, 5:]
+fig = plt.figure()
+plt.plot(data)
+plt.xlabel('time (min.)')
+plt.ylabel('normalized fluorescence (measure of expression)')
+plt.show()
