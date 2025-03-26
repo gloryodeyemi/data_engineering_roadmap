f3cf41f82ff245b92671938ec1a92a85cb3859aa
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-26 16:54:24 -0400

    App Click-through Rate (CTR)

--- Data_Lemur/app_ctr.sql
+++ Data_Lemur/app_ctr.sql
@@ -41,7 +41,18 @@ SELECT
   ROUND(
     100.0 * SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) /
     SUM(CASE WHEN event_type = 'impression' THEN 1 ELSE 0 END), 2
-  )
+  ) AS ctr
 FROM events
 WHERE EXTRACT(YEAR FROM timestamp ) = '2022'
+GROUP BY app_id;
+
+-- USING FILTER
+SELECT
+  app_id,
+  ROUND(100.0 *
+    SUM(1) FILTER (WHERE event_type = 'click') /
+    SUM(1) FILTER (WHERE event_type = 'impression'), 2) AS ctr_app
+FROM events
+WHERE timestamp >= '2022-01-01' 
+  AND timestamp < '2023-01-01'
 GROUP BY app_id;
