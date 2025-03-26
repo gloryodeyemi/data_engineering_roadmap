1c75554d19e9e452802c6001771522c63ace5123
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-03-25 22:24:54 -0400

    querying the database

--- Datacamp/DEP_Track/database.py
+++ Datacamp/DEP_Track/database.py
@@ -91,3 +91,80 @@ with engine.connect() as con:
 
 # Print head of DataFrame
 print(df.head())
+
+"""
+Import the pandas package using the alias pd.
+Using the function create_engine(), create an engine for the SQLite database Chinook.sqlite and assign it to the variable engine.
+Use the pandas function read_sql_query() to assign to the variable df the DataFrame of results from the following query: select all records 
+from the table Album.
+The remainder of the code is included to confirm that the DataFrame created by this method is equal to that created by the previous method t
+hat you learned.
+"""
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine("sqlite:///Chinook.sqlite")
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM Album", engine)
+print(df.head())
+
+# Open engine in context manager and store query result in df1
+with engine.connect() as con:
+    rs = con.execute("SELECT * FROM Album")
+    df1 = pd.DataFrame(rs.fetchall())
+    df1.columns = rs.keys()
+
+# Confirm that both methods yield the same result
+print(df.equals(df1))
+
+"""
+Using the function create_engine(), create an engine for the SQLite database Chinook.sqlite and assign it to the variable engine.
+Use the pandas function read_sql_query() to assign to the variable df the DataFrame of results from the following query: select all records 
+from the Employee table where the EmployeeId is greater than or equal to 6 and ordered by BirthDate (make sure to use WHERE and ORDER BY in 
+this precise order).
+"""
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Execute query and store records in DataFrame: df
+df = pd.read_sql_query("SELECT * FROM Employee WHERE EmployeeId >= 6 ORDER BY BirthDate", engine)
+
+# Print head of DataFrame
+print(df.head())
+
+"""
+Assign to rs the results from the following query: select all the records, extracting the Title of the record and Name of the artist of each 
+record from the Album table and the Artist table, respectively. To do so, INNER JOIN these two tables on the ArtistID column of both.
+In a call to pd.DataFrame(), apply the method fetchall() to rs in order to fetch all records in rs. Store them in the DataFrame df.
+Set the DataFrame's column names to the corresponding names of the table columns.
+"""
+with engine.connect() as con:
+    rs = con.execute("SELECT Title, Name FROM Album INNER JOIN Artist ON Album.ArtistID = Artist.ArtistID")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
+
+"""
+Use the pandas function read_sql_query() to assign to the variable df the DataFrame of results from the following query: select all records 
+from PlaylistTrack INNER JOIN Track on PlaylistTrack.TrackId = Track.TrackId that satisfy the condition Milliseconds < 250000
+"""
+df = pd.read_sql_query(
+    """
+    SELECT * 
+    FROM PlaylistTrack 
+    INNER JOIN Track 
+    ON PlaylistTrack.TrackId = Track.TrackId 
+    WHERE Milliseconds < 250000
+    """, 
+    engine
+)
+
+# Print head of DataFrame
+print(df.head())
