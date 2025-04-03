8a1ec22d24f368253f61f69f1bef3fe794136d5a
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-03 13:52:28 -0400

    Importing data from the web

--- Datacamp/DEP_Track/importing_from_web.py
+++ Datacamp/DEP_Track/importing_from_web.py
@@ -99,3 +99,73 @@ r = requests.get(url)
 
 text = r.text
 print(text)
+
+"""
+Import the function BeautifulSoup from the package bs4.
+Assign the URL of interest to the variable url.
+Package the request to the URL, send the request and catch the response with a single function requests.get(), assigning the response to the 
+variable r.
+Use the text attribute of the object r to return the HTML of the webpage as a string; store the result in a variable html_doc.
+Create a BeautifulSoup object soup from the resulting HTML using the function BeautifulSoup().
+Use the method prettify() on soup and assign the result to pretty_soup.
+Hit submit to print to prettified HTML to your shell!
+"""
+# Import packages
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = 'https://www.python.org/~guido/'
+r = requests.get(url)
+
+# Extracts the response as html: html_doc
+html_doc = r.text
+soup = BeautifulSoup(html_doc)
+pretty_soup = soup.prettify()
+print(pretty_soup)
+
+"""
+In the sample code, the HTML response object html_doc has already been created: your first task is to Soupify it using the function 
+BeautifulSoup() and to assign the resulting soup to the variable soup.
+Extract the title from the HTML soup soup using the attribute title and assign the result to guido_title.
+Print the title of Guido's webpage to the shell using the print() function.
+Extract the text from the HTML soup soup using the method get_text() and assign to guido_text.
+Hit submit to print the text from Guido's webpage to the shell.
+Use the method find_all() to find all hyperlinks in soup, remembering that hyperlinks are defined by the HTML tag <a> but passed to 
+find_all() without angle brackets; store the result in the variable a_tags.
+The variable a_tags is a results set: your job now is to enumerate over it, using a for loop and to print the actual URLs of the hyperlinks; 
+to do this, for every element link in a_tags, you want to print() link.get('href').
+"""
+import requests
+from bs4 import BeautifulSoup
+
+# Specify url: url
+url = 'https://www.python.org/~guido/'
+r = requests.get(url)
+html_doc = r.text
+
+soup = BeautifulSoup(html_doc)
+guido_title = soup.title
+print(guido_title)
+
+guido_text = soup.text
+print(guido_text)
+
+a_tags = soup.find_all('a')
+
+for link in a_tags:
+    print(link.get('href'))
+
+"""
+Load the JSON 'a_movie.json' into the variable json_data within the context provided by the with statement. To do so, use the function 
+json.load() within the context manager.
+Use a for loop to print all key-value pairs in the dictionary json_data. Recall that you can access a value in a dictionary using the 
+syntax: dictionary[key].
+"""
+# Load JSON: json_data
+with open("a_movie.json") as json_file:
+    json_data = json.load(json_file)
+
+# Print each key-value pair in json_data
+for k in json_data.keys():
+    print(k + ': ', json_data[k])
