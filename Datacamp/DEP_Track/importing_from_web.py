2c5f629f590409ba4dd33eb4228837d0d1197036
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-03 13:26:07 -0400

    Importing data from the web

--- Datacamp/DEP_Track/importing_from_web.py
+++ Datacamp/DEP_Track/importing_from_web.py
@@ -48,3 +48,54 @@ url = 'https://assets.datacamp.com/course/importing_data_into_r/latitude.xls'
 xls = pd.read_excel(url, sheet_name=None)
 print(xls.keys())
 print(xls['1700'].head())
+
+"""
+Import the functions urlopen and Request from the subpackage urllib.request.
+Package the request to the url "https://campus.datacamp.com/courses/1606/4135?ex=2" using the function Request() and assign it to request.
+Send the request and catch the response in the variable response with the function urlopen().
+Run the rest of the code to see the datatype of response and to close the connection!
+"""
+from urllib.request import urlopen, Request
+
+# Specify the url
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+
+# This packages the request: request
+request = Request(url)
+
+# Sends the request and catches the response: response
+response = urlopen(request)
+print(type(response))
+response.close()
+
+"""
+Send the request and catch the response in the variable response with the function urlopen(), as in the previous exercise.
+Extract the response using the read() method and store the result in the variable html.
+Print the string html.
+Hit submit to perform all of the above and to close the response: be tidy!
+"""
+from urllib.request import urlopen, Request
+
+url = "https://campus.datacamp.com/courses/1606/4135?ex=2"
+request = Request(url)
+response = urlopen(request)
+
+html = response.read()
+print(html)
+response.close()
+
+"""
+Import the package requests.
+Assign the URL of interest to the variable url.
+Package the request to the URL, send the request and catch the response with a single function requests.get(), assigning the response to the 
+variable r.
+Use the text attribute of the object r to return the HTML of the webpage as a string; store the result in a variable text.
+Hit submit to print the HTML of the webpage.
+"""
+import requests
+
+url = "http://www.datacamp.com/teach/documentation"
+r = requests.get(url)
+
+text = r.text
+print(text)
