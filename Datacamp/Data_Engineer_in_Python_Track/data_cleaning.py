ec85af55f0b76973054e56b19ea0e1ff8fa2ee14
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-21 18:33:51 -0400

    Data cleaning

--- Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
@@ -64,3 +64,33 @@ duplicated_rides = ride_unique[duplicates == True]
 
 # Assert duplicates are processed
 assert duplicated_rides.shape[0] == 0
+
+"""
+Print the categories DataFrame and take a close look at all possible correct categories of the survey columns.
+Print the unique values of the survey columns in airlines using the .unique() method.
+"""
+# Print categories DataFrame
+print(categories)
+
+# Print unique values of survey columns in airlines
+print('Cleanliness: ', airlines['cleanliness'].unique(), "\n")
+print('Safety: ', airlines['safety'].unique(), "\n")
+print('Satisfaction: ', airlines['satisfaction'].unique(), "\n")
+
+"""
+Create a set out of the cleanliness column in airlines using set() and find the inconsistent category by finding the difference in the 
+cleanliness column of categories.
+Find rows of airlines with a cleanliness value not in categories and print the output.
+Print the rows with the consistent categories of cleanliness only.
+"""
+# Find the cleanliness category in airlines not in categories
+cat_clean = set(airlines['cleanliness']).difference(categories['cleanliness'])
+
+# Find rows with that category
+cat_clean_rows = airlines['cleanliness'].isin(cat_clean)
+
+# Print rows with inconsistent category
+print(airlines[cat_clean_rows])
+
+# Print rows with consistent categories only
+print(airlines[~cat_clean_rows])
