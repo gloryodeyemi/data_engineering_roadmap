1182ed98705189f30770625cb4ca929cffd60600
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-21 19:32:45 -0400

    Data cleaning

--- Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
@@ -94,3 +94,46 @@ print(airlines[cat_clean_rows])
 
 # Print rows with consistent categories only
 print(airlines[~cat_clean_rows])
+
+
+"""
+Change the capitalization of all values of dest_region to lowercase.
+Replace the 'eur' with 'europe' in dest_region using the .replace() method.
+Strip white spaces from the dest_size column using the .strip() method.
+Verify that the changes have been into effect by printing the unique values of the columns using .unique() .
+"""
+# Print unique values of both columns
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+# Lower dest_region column and then replace "eur" with "europe"
+airlines['dest_region'] = airlines['dest_region'].str.lower()
+airlines['dest_region'] = airlines['dest_region'].replace({'eur':'europe'})
+
+# Remove white spaces from `dest_size`
+airlines['dest_size'] = airlines['dest_size'].str.strip()
+
+# Verify changes have been effected
+print(airlines['dest_region'].unique())
+print(airlines['dest_size'].unique())
+
+"""
+Create the ranges and labels for the wait_type column mentioned in the description.
+Create the wait_type column by from wait_min by using pd.cut(), while inputting label_ranges and label_names in the correct arguments.
+Create the mapping dictionary mapping weekdays to 'weekday' and weekend days to 'weekend'.
+Create the day_week column by using .replace().
+"""
+# Create ranges for categories
+label_ranges = [0, 60, 180, np.inf]
+label_names = ['short', 'medium', 'long']
+
+# Create wait_type column
+airlines['wait_type'] = pd.cut(airlines['wait_min'], bins = label_ranges, 
+                                labels = label_names)
+
+# Create mappings and replace
+mappings = {'Monday':'weekday', 'Tuesday':'weekday', 'Wednesday': 'weekday', 
+            'Thursday': 'weekday', 'Friday': 'weekday', 
+            'Saturday': 'weekend', 'Sunday': 'weekend'}
+
+airlines['day_week'] = airlines['day'].replace(mappings)
