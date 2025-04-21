5f1ee45509e0cfe0458e42b224496dda9be10c20
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-21 17:42:07 -0400

    Data cleaning

--- Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
@@ -33,3 +33,34 @@ ride_sharing['ride_dt'] = pd.to_datetime(ride_sharing['ride_date']).dt.date
 today = dt.date.today()
 ride_sharing.loc[ride_sharing['ride_dt'] > today, 'ride_dt'] = today
 print(ride_sharing['ride_dt'].max())
+
+"""
+Find duplicated rows of ride_id in the ride_sharing DataFrame while setting keep to False.
+Subset ride_sharing on duplicates and sort by ride_id and assign the results to duplicated_rides.
+Print the ride_id, duration and user_birth_year columns of duplicated_rides in that order.
+"""
+duplicates = ride_sharing.duplicated(subset=['ride_id'], keep=False)
+duplicated_rides = ride_sharing[duplicates].sort_values(by='ride_id')
+print(duplicated_rides[['ride_id','duration','user_birth_year']])
+
+"""
+Drop complete duplicates in ride_sharing and store the results in ride_dup.
+Create the statistics dictionary which holds minimum aggregation for user_birth_year and mean aggregation for duration.
+Drop incomplete duplicates by grouping by ride_id and applying the aggregation in statistics.
+Find duplicates again and run the assert statement to verify de-duplication.
+"""
+# Drop complete duplicates from ride_sharing
+ride_dup = ride_sharing.drop_duplicates()
+
+# Create statistics dictionary for aggregation function
+statistics = {'user_birth_year': 'min', 'duration': 'mean'}
+
+# Group by ride_id and compute new statistics
+ride_unique = ride_dup.groupby('ride_id').agg(statistics).reset_index()
+
+# Find duplicated values again
+duplicates = ride_unique.duplicated(subset = 'ride_id', keep = False)
+duplicated_rides = ride_unique[duplicates == True]
+
+# Assert duplicates are processed
+assert duplicated_rides.shape[0] == 0
