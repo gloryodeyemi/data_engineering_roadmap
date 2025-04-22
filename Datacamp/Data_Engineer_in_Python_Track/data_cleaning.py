a51d16d3f2b7a19d95ff568b034e0528e09f163e
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-22 02:44:35 -0400

    Data cleaning

--- Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
@@ -282,3 +282,65 @@ banking_imputed = banking_fullid.fillna({'acct_amount': acct_imp})
 
 # Print number of missing values
 print(banking_imputed.isna().sum())
+
+"""
+Import process from thefuzz.
+Store the unique cuisine_types into unique_types.
+Calculate the similarity of 'asian', 'american', and 'italian' to all possible cuisine_types using process.extract(), while returning all 
+possible matches.
+"""
+# Import process from thefuzz
+from thefuzz import process
+
+# Store the unique values of cuisine_type in unique_types
+unique_types = restaurants['cuisine_type'].unique()
+
+# Calculate similarity of 'asian' to all values of unique_types
+print(process.extract('asian', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'american' to all values of unique_types
+print(process.extract('american', unique_types, limit = len(unique_types)))
+
+# Calculate similarity of 'italian' to all values of unique_types
+print(process.extract('italian', unique_types, limit = len(unique_types)))
+
+"""
+As a first step, create a list of all possible matches, comparing 'italian' with the restaurant types listed in the cuisine_type column.
+"""
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants))
+print(matches[0:5])
+
+"""
+Within the for loop, use an if statement to check whether the similarity score in each match is greater than or equal to 80.
+If it is, use .loc to select rows where cuisine_type in restaurants is equal to the current match (which is the first element of match), and 
+reassign them to be 'italian'.
+"""
+# Create a list of matches, comparing 'italian' with the cuisine_type column
+matches = process.extract('italian', restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+# Iterate through the list of matches to italian
+for match in matches:
+  # Check whether the similarity score is greater than or equal to 80
+  if match[1] >= 80:
+    # Select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+    restaurants.loc[restaurants['cuisine_type'] == match[0], 'cuisine_type'] = 'italian'
+
+
+"""
+Using the variable cuisine to iterate through categories, embed your code from the previous step in an outer for loop.
+"""
+# Iterate through categories
+for cuisine in categories:  
+  # Create a list of matches, comparing cuisine with the cuisine_type column
+  matches = process.extract(cuisine, restaurants['cuisine_type'], limit=len(restaurants.cuisine_type))
+
+  # Iterate through the list of matches
+  for match in matches:
+     # Check whether the similarity score is greater than or equal to 80
+    if match[1] >= 80:
+      # If it is, select all rows where the cuisine_type is spelled this way, and set them to the correct cuisine
+      restaurants.loc[restaurants['cuisine_type'] == match[0]] = cuisine
+      
+# Inspect the final result
+print(restaurants['cuisine_type'].unique())
