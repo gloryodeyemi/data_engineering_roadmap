dfb81ea827d82d378a017ba7a3b68f10d127cf80
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-24 15:52:00 -0400

    Data cleaning

--- Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
@@ -381,3 +381,22 @@ comp_cl.string('rest_name', 'rest_name', label='name', threshold = 0.8)
 # Get potential matches and print
 potential_matches = comp_cl.compute(pairs, restaurants, restaurants_new)
 print(potential_matches)
+
+"""
+Isolate instances of potential_matches where the row sum is above or equal to 3 by using the .sum() method.
+Extract the second column index from matches, which represents row indices of matching record from restaurants_new by using the .get_level_values() method.
+Subset restaurants_new for rows that are not in matching_indices.
+Append non_dup to restaurants.
+"""
+# Isolate potential matches with row sum >=3
+matches = potential_matches[potential_matches.sum(axis = 1) >= 3]
+
+# Get values of second column index of matches
+matching_indices = matches.index.get_level_values(1)
+
+# Subset restaurants_new based on non-duplicate values
+non_dup = restaurants_new[~restaurants_new.index.isin(matching_indices)]
+
+# Append non_dup to restaurants
+full_restaurants = restaurants.append(non_dup)
+print(full_restaurants)
