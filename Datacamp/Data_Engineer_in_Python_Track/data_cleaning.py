3b3e39123e7fc01c467bfc59479317cced8b4cd6
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-24 15:42:00 -0400

    Data cleaning

--- Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_cleaning.py
@@ -344,3 +344,40 @@ for cuisine in categories:
       
 # Inspect the final result
 print(restaurants['cuisine_type'].unique())
+
+"""
+Instantiate an indexing object by using the Index() function from recordlinkage.
+Block your pairing on cuisine_type by using indexer's' .block() method.
+Generate pairs by indexing restaurants and restaurants_new in that order.
+"""
+# Create an indexer and object and find possible pairs
+indexer = recordlinkage.Index()
+
+# Block pairing on cuisine_type
+indexer.block('cuisine_type')
+
+# Generate pairs
+pairs = indexer.index(restaurants, restaurants_new)
+
+"""
+Instantiate a comparison object using the recordlinkage.Compare() function.
+Use the appropriate comp_cl method to find exact matches between the city and cuisine_type columns of both DataFrames.
+Use the appropriate comp_cl method to find similar strings with a 0.8 similarity threshold in the rest_name column of both DataFrames.
+Compute the comparison of the pairs by using the .compute() method of comp_cl.
+"""
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Create a comparison object
+comp_cl = recordlinkage.Compare()
+
+# Find exact matches on city, cuisine_types 
+comp_cl.exact('city', 'city', label='city')
+comp_cl.exact('cuisine_type', 'cuisine_type', label = 'cuisine_type')
+
+# Find similar matches of rest_name
+comp_cl.string('rest_name', 'rest_name', label='name', threshold = 0.8) 
+
+# Get potential matches and print
+potential_matches = comp_cl.compute(pairs, restaurants, restaurants_new)
+print(potential_matches)
