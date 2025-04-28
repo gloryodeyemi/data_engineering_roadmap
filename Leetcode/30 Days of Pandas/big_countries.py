06014696c5affd466b5bcf9549c6e976a6a8ced8
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-28 18:33:36 -0400

    Big Countries

--- Leetcode/30 Days of Pandas/big_countries.py
+++ Leetcode/30 Days of Pandas/big_countries.py
@@ -48,3 +48,9 @@ Output:
 | Algeria     | 37100000   | 2381741 |
 +-------------+------------+---------+
 """
+import pandas as pd
+
+def big_countries(world: pd.DataFrame) -> pd.DataFrame:
+    big_countries = world[(world['area'] >= 3000000) | (world['population'] >= 25000000)]
+    return big_countries[['name', 'population', 'area']]
+  
