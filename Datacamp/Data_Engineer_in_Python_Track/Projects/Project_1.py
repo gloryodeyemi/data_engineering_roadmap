edaaa2a44b23ddf26a79631e8928b9a6336079e4
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-28 20:08:32 -0400

    Cleaning Bank Marketing Campaign Data

--- Datacamp/Data_Engineer_in_Python_Track/Projects/Project_1.py
+++ Datacamp/Data_Engineer_in_Python_Track/Projects/Project_1.py
@@ -7,3 +7,46 @@ Subset, clean, and reformat the bank_marketing.csv dataset to create and store t
 notebook and formatted to the data types listed.
 - Save the three DataFrames to csv files, without an index, as client.csv, campaign.csv, and economics.csv respectively.
 """
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("bank_marketing.csv")
+
+# Clients DataFrame
+# 1. job column - Change "." to "_"
+df['job'] = df['job'].str.replace(".", "_")
+
+# 2. education column - Change "." to "_" and "unknown" to np.NaN
+df['education'] = df['education'].str.replace('.', '_', regex=False).replace('unknown', np.nan)
+
+# 3. credit_default column - Convert to boolean data type: 1 if "yes", otherwise 0
+df['credit_default'] = df['credit_default'].replace({"yes": 1, "no": 0}).astype('bool')
+
+# 4. mortgage column - Convert to boolean data type: 1 if "yes", otherwise 0
+df['mortgage'] = df['mortgage'].replace({"yes": 1, "no": 0}).astype('bool')
+
+# create client DataFrame
+client = df[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']]
+print(client.head())
+
+# save client DataFrame as csv file
+client.to_csv('client.csv', index=False)
+
+# Campaign DataFrame
+# 1. previous_outcome column - Convert to boolean data type: 1 if "success", otherwise 0.
+df['previous_outcome'] = df['previous_outcome'].apply(lambda x: 1 if x == 'success' else 0).astype('bool')
+
+# 2. campaign_outcome column - Convert to boolean data type: 1 if "yes", otherwise 0.
+df['campaign_outcome'] = df['campaign_outcome'].apply(lambda x: 1 if x == 'yes' else 0).astype('bool')
+
+# 3. last_contact_date column - Create from a combination of day, month, and a newlycreated year column (whichshould have a value of 2022); Format = "YYYY-MM-DD"
+df['year'] = 2022
+df['last_contact_date'] = df['day'].astype(str) + '-' + df['month'] + '-' + df['year'].astype(str)
+df['last_contact_date'] = pd.to_datetime(df['last_contact_date'], format='%d-%b-%Y')
+# df['last_contact_date'] = pd.to_datetime(df[['year', 'month', 'day']])
+
+campaign = df[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome', 'last_contact_date']]
+print(campaign.head())
+
+# 6. save campaign DataFrame as csv file
+campaign.to_csv('campaign.csv', index=False)
