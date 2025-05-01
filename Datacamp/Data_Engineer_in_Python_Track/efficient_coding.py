c0730830d318be5e4eb12b8c929d8a590e369e81
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-04-30 22:37:45 -0400

    Efficient Python Code

--- Datacamp/Data_Engineer_in_Python_Track/efficient_coding.py
+++ Datacamp/Data_Engineer_in_Python_Track/efficient_coding.py
@@ -83,5 +83,45 @@ Replace the third column in nums with a new column that adds 1 to each item in t
 """
 import numpy as np
 
-arr = np.arange(1, 11).reshape(2, 5)
+nums = np.arange(1, 11).reshape(2, 5)
 
+# Print second row of nums
+print(nums[1])
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+
+# Replace the third column of nums
+nums[:, 2] = nums[:, 2] + 1
+
+print(nums)
+
+"""
+Use range() to create a list of arrival times (10 through 50 incremented by 10). Create the list arrival_times by unpacking the range object.
+
+You realize your clock is three minutes fast. Convert the arrival_times list into a numpy array (called arrival_times_np) and use NumPy broadcasting to subtract three 
+minutes from each arrival time.
+
+Use list comprehension with enumerate() to pair each guest in the names list to their updated arrival time in the new_times array. You'll need to use the index variable 
+created from using enumerate() on new_times to index the names list.
+
+A function named welcome_guest() has been pre-loaded into your session. Use map() to apply this function to each element of the guest_arrivals list and save it as the variable welcome_map.
+"""
+arrival_times = [*range(10, 60, 10)]
+
+# Convert arrival_times to an array and update the times
+arrival_times_np = np.array(arrival_times)
+new_times = arrival_times_np - 3
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[i],time) for i,time in enumerate(new_times)]
+
+# Map the welcome_guest function to each (guest,time) pair
+welcome_map = map(welcome_guest, guest_arrivals)
+
+guest_welcomes = [*welcome_map]
+print(*guest_welcomes, sep='\n')
