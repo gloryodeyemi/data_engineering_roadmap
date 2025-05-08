fa58ecdc16c2064e6806bafff122ca12e9255b68
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-05-08 04:12:41 -0400

    Writing efficient code in python

--- Datacamp/Data_Engineer_in_Python_Track/efficient_coding2.py
+++ Datacamp/Data_Engineer_in_Python_Track/efficient_coding2.py
@@ -44,3 +44,83 @@ def convert_units_broadcast(heroes, heights, weights):
 
 # %load_ext line_profiler
 # %lprun -f convert_units_broadcast convert_units_broadcast(heroes, hts, wts)
+
+def calc_bmi_lists(sample_indices, hts, wts):
+
+    # Gather sample heights and weights as lists
+    s_hts = [hts[i] for i in sample_indices]
+    s_wts = [wts[i] for i in sample_indices]
+
+    # Convert heights from cm to m and square with list comprehension
+    s_hts_m_sqr = [(ht / 100) ** 2 for ht in s_hts]
+
+    # Calculate BMIs as a list with list comprehension
+    bmis = [s_wts[i] / s_hts_m_sqr[i] for i in range(len(sample_indices))]
+
+    return bmis
+
+"""
+Load the memory_profiler package into your IPython session.
+Import calc_bmi_lists from bmi_lists.
+Once you've completed the above steps, use %mprun to profile the calc_bmi_lists() function acting on your superheroes data. The hts array and 
+wts array have already been loaded into your session.
+"""
+# %load_ext memory_profiler
+# from bmi_lists import calc_bmi_lists
+
+# %mprun -f calc_bmi_lists calc_bmi_lists(sample_indices, hts, wts)
+
+def calc_bmi_arrays(sample_indices, hts, wts):
+
+    # Gather sample heights and weights as arrays
+    s_hts = hts[sample_indices]
+    s_wts = wts[sample_indices]
+
+    # Convert heights from cm to m and square with broadcasting
+    s_hts_m_sqr = (s_hts / 100) ** 2
+
+    # Calculate BMIs as an array using broadcasting
+    bmis = s_wts / s_hts_m_sqr
+
+    return bmis
+
+# %load_ext memory_profiler
+# from bmi_arrays import calc_bmi_arrays
+
+# %mprun -f calc_bmi_arrays calc_bmi_arrays(sample_indices, hts, wts)
+
+def get_publisher_heroes(heroes, publishers, desired_publisher):
+
+    desired_heroes = []
+
+    for i,pub in enumerate(publishers):
+        if pub == desired_publisher:
+            desired_heroes.append(heroes[i])
+
+    return desired_heroes
+def get_publisher_heroes_np(heroes, publishers, desired_publisher):
+
+    heroes_np = np.array(heroes)
+    pubs_np = np.array(publishers)
+
+    desired_heroes = heroes_np[pubs_np == desired_publisher]
+
+    return desired_heroes
+
+"""
+Use the get_publisher_heroes() function and the get_publisher_heroes_np() function to collect heroes from the Star Wars universe. 
+The desired_publisher for Star Wars is 'George Lucas'.
+"""
+# Use get_publisher_heroes() to gather Star Wars heroes
+star_wars_heroes = get_publisher_heroes(heroes, publishers, 'George Lucas')
+print(star_wars_heroes)
+print(type(star_wars_heroes))
+
+# Use get_publisher_heroes_np() to gather Star Wars heroes
+star_wars_heroes_np = get_publisher_heroes_np(heroes, publishers, 'George Lucas')
+print(star_wars_heroes_np)
+print(type(star_wars_heroes_np))
+
+# %load_ext line_profiler
+# %lprun -f get_publisher_heroes get_publisher_heroes(heroes, publishers, 'George Lucas')
+# %lprun -f get_publisher_heroes_np get_publisher_heroes_np(heroes, publishers, 'George Lucas')
