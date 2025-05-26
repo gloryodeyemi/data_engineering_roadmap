a4907b407846efa4c47f7f271e06dcc6a3040b91
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-05-26 11:34:12 -0400

    Remove Element

---
+++ Leetcode/Top Interview 150/Easy/remove_element.py
@@ -0,0 +1,17 @@
+"""
+Remove Element
+
+
+"""
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        while val in nums:
+            nums.remove(val)
+        k = len(nums)
+        return k
+        
