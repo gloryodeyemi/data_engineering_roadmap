99be083a8dada0ac1f2655603e3c8c5c8a29c80f
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-06-04 16:57:29 -0400

    Average Time of Process per Machine

--- Leetcode/SQL 50/Easy/avg_time_process.sql
+++ Leetcode/SQL 50/Easy/avg_time_process.sql
@@ -23,9 +23,11 @@ The 'start' timestamp will always be before the 'end' timestamp for every (machi
 It is guaranteed that each (machine_id, process_id) pair has a 'start' and 'end' timestamp.
  
 
-There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each machine takes to complete a process.
+There is a factory website that has several machines each running the same number of processes. Write a solution to find the average time each 
+machine takes to complete a process.
 
-The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete every process on the machine divided by the number of processes that were run.
+The time to complete a process is the 'end' timestamp minus the 'start' timestamp. The average time is calculated by the total time to complete 
+every process on the machine divided by the number of processes that were run.
 
 The resulting table should have the machine_id along with the average time as processing_time, which should be rounded to 3 decimal places.
 
