54cabab434a205070af46f97fa52d8536395f211
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-06-03 20:00:59 -0400

    Find the Runner-Up Score!

--- HackerRank/Python/Easy/runner_up_score.py
+++ HackerRank/Python/Easy/runner_up_score.py
@@ -25,3 +25,30 @@ Sample Output 0
 Explanation 0
 Given list is [2, 3, 6, 6, 5]. The maximum score is 6, second maximum is 5. Hence, we print 5 as the runner-up score.
 """
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    arr = list(arr)
+    max_score = max(arr)
+    runner_up = 0
+    
+    for i in range(n):
+        if arr[i] > runner_up and arr[i] < max_score:
+            runner_up = arr[i]
+    print(runner_up)
+
+# solution 2
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    arr = list(arr)
+    max_score = max(arr)
+    runner_up = float('-inf')
+    
+    for score in arr:
+        if score > runner_up and score < max_score:
+            runner_up = score
+    print(runner_up)
+    
