629dc2c91b67fc67f79741df4cab4bb6ac543aad
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-06-09 18:25:54 -0400

    Data ingestion with pandas

--- Datacamp/Data_Engineer_in_Python_Track/data_ingestion.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_ingestion.py
@@ -56,3 +56,58 @@ vt_data_next500 = pd.read_csv("vt_tax_data_2016.csv",
 # View the Vermont dataframes to confirm they're different
 print(vt_data_first500.head())
 print(vt_data_next500.head())
+
+"""
+Load vt_tax_data_2016.csv with no arguments and view the dataframe's dtypes attribute. Note the data types of zipcode and agi_stub.
+"""
+# Load csv with no additional arguments
+data = pd.read_csv("vt_tax_data_2016.csv")
+
+# Print the data types
+print(data.dtypes)
+
+"""
+Create a dictionary, data_types, specifying that agi_stub is "category" data and zipcode is string data.
+Reload the CSV with the dtype argument and the dictionary to set the correct column data types.
+View the dataframe's dtypes attribute.
+"""
+# Create dict specifying data types for agi_stub and zipcode
+data_types = {"agi_stub":"category",
+			  "zipcode":str}
+
+# Load csv using dtype to set correct data types
+data = pd.read_csv("vt_tax_data_2016.csv", dtype=data_types)
+
+# Print data types of resulting frame
+print(data.dtypes.head())
+
+"""
+Create a dictionary, null_values, specifying that 0s in the zipcode column should be considered NA values.
+Load vt_tax_data_2016.csv, using the na_values argument and the dictionary to make sure invalid ZIP codes are treated as missing.
+"""
+# Create dict specifying that 0s in zipcode are NA values
+null_values = {"zipcode": 0}
+
+# Load csv using na_values keyword argument
+data = pd.read_csv("vt_tax_data_2016.csv", 
+                   na_values=null_values)
+
+# View rows with NA ZIP codes
+print(data[data.zipcode.isna()])
+
+"""
+Try to import the file vt_tax_data_2016_corrupt.csv without any keyword arguments.
+Import vt_tax_data_2016_corrupt.csv with the error_bad_lines parameter set to skip bad records.
+Update the import with the warn_bad_lines parameter set to issue a warning whenever a bad record is skipped.
+"""
+try:
+  # Set warn_bad_lines to issue warnings about bad records
+  data = pd.read_csv("vt_tax_data_2016_corrupt.csv", 
+                     error_bad_lines=False, 
+                     warn_bad_lines=True)
+  
+  # View first 5 records
+  print(data.head())
+  
+except pd.errors.ParserError:
+    print("Your data contained rows that could not be parsed.")
