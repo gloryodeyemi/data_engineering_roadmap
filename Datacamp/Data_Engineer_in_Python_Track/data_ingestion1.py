73c6517a2218ac4bd65594ea4819220850266238
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-06-09 19:47:44 -0400

    Data ingestion with pandas

--- Datacamp/Data_Engineer_in_Python_Track/data_ingestion1.py
+++ Datacamp/Data_Engineer_in_Python_Track/data_ingestion1.py
@@ -262,7 +262,6 @@ View summary statistics about the new Part2Start column with the describe() meth
 # Create dict of columns to combine into new datetime column
 datetime_cols = {"Part2Start": ['Part2StartDate', 'Part2StartTime']}
 
-
 # Load file, supplying the dict to parse_dates
 survey_data = pd.read_excel("fcc_survey_dts.xlsx",
                             parse_dates=datetime_cols)
