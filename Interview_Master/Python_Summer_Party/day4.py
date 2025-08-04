1874e2fb6b9120d4bef9f5f96757ad8112e6c32d
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-08-04 18:25:52 -0400

    Python Party Day 4: Search Results Page: User Interaction Patterns

---
+++ Interview_Master/Python_Summer_Party/day4.py
@@ -0,0 +1,7 @@
+"""
+Python Party Day 4: Search Results Page: User Interaction Patterns
+
+You are a Product Analyst on the Google Search team investigating user engagement with search result pages. The team wants to understand how 
+different numbers of search results impact user interaction time. Your analysis will help optimize the current search results presentation 
+strategy.
+"""
