15b126bb093ccf7974610025d0467c7f646ad85d
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-08-10 20:37:52 -0400

    Python Party Day 10: App Store Ratings Performance by App Category

---
+++ Interview_Master/Python_Summer_Party/day10.py
@@ -0,0 +1,6 @@
+"""
+Python Party Day 10: App Store Ratings Performance by App Category
+
+You are a Product Analyst for the Apple App Store team investigating app ratings data. Your focus is to clean and understand rating distributions 
+across different app categories. The team wants to leverage basic statistical insights to guide app performance strategies.
+"""
