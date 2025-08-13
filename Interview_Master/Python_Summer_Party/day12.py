d36d85676797dbb4e7fcf478fcda41949cc82c55
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2025-08-12 22:41:54 -0400

    Python Party Day 12: E-commerce Returns Customer Segmentation Model

---
+++ Interview_Master/Python_Summer_Party/day12.py
@@ -0,0 +1,11 @@
+"""
+Python Party Day 12: E-commerce Returns Customer Segmentation Model
+
+You are a Data Analyst on the Walmart.com Insights team investigating customer return patterns. The team aims to develop a predictive approach to understanding customer return behaviors across different time periods. Your goal is to leverage transaction data to create a comprehensive view of customer return likelihood.
+"""
+
+"""
+Question 1 of 2
+---------------
+Identify and list all unique customer IDs who have made returns between July 1st 2024 and June 30th 2025. This will help us understand the base set of customers involved in returns during the specified period.
+"""
